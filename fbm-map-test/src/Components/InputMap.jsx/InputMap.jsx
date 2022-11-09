cdf099b911ded5fb3aa02a11ca431a0ff8ab7b43
Joe <joefuller042@gmail.com>
2022-11-09 11:05:00 +0000

    fixed map

--- fbm-map-test/src/Components/InputMap.jsx/InputMap.jsx
+++ fbm-map-test/src/Components/InputMap.jsx/InputMap.jsx
@@ -1,31 +1,34 @@
-import {useState} from 'react';
-import { MapContainer, TileLayer, Marker } from 'react-leaflet'
+import { useState } from "react";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
 
 export const InputMap = (props) => {
-    const {data} = props;
-    const testData = [{location: [50,50]}];
-    // const [clickedPos, setClickedPos] = useState(null);
-    // const handleClick = (e) => {
-    // //    setClickedPos({ location: e.latlng });
-    //    console.log(e.latlng);
-    //    console.log(e);
-    //   };
+  const { data } = props;
+  const testData = [{ location: [50, 50] }];
+  // const [clickedPos, setClickedPos] = useState(null);
+  // const handleClick = (e) => {
+  // //    setClickedPos({ location: e.latlng });
+  //    console.log(e.latlng);
+  //    console.log(e);
+  //   };
 
-    return (
-    <div className="input-map">
-        <MapContainer center={[0, 0]} zoom={1} scrollWheelZoom={false} height={500}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {testData.map((d) => {
-            console.log(d);
-            return  <Marker key={1} position={d.location}> </Marker>
-            })}
-
-
-      </MapContainer>
-      </div>)
-}
+  return (
+    // <div className="input-map">
+    <MapContainer center={[0, 0]} zoom={1} scrollWheelZoom={false} height={500}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {testData.map((d) => {
+        console.log(d);
+        return (
+          <Marker key={1} position={d.location}>
+            {" "}
+          </Marker>
+        );
+      })}
+    </MapContainer>
+    // </div>
+  );
+};
 
 export default InputMap;
